a12782e22fe7b0d33047fda23c3fc3fee0f9c6bf
hdgithub123 <163997843+hdgithub123@users.noreply.github.com>
2026-02-13 10:20:22 +0700

    build file

---
+++ buildRain.bat
@@ -0,0 +1,3 @@
+@echo off
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /target:winexe /win32icon:RainStormIcon.ico /out:RainFallDesktop.exe RainDesktop.cs
+pause
---
+++ buildSnow.bat
@@ -0,0 +1,3 @@
+@echo off
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /target:winexe /win32icon:appicon.ico /out:SnowFallDesktop.exe SnowDesktop.cs
+pause
